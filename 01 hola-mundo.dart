3eb37448aea7d253a7fdf5798acf731a1f3d8464
camilocaquimbo <ingcamiloc@gmail.com>
2020-09-07 23:06:06 -0500

    Init

---
+++ 01 hola-mundo.dart
@@ -0,0 +1,14 @@
+void main() {
+  print('Hola mundo');
+
+  // Comment
+
+  /* Comentario bloque
+    Bloque
+   */
+
+  //var nombre = 'Camilo';
+  String nombre = 'Camilo_S';
+
+  print('Hola $nombre');
+}
---
+++ 02 numeros.dart
@@ -0,0 +1,14 @@
+void main() {
+  //Números:
+
+  int empleados = 10;
+  double pi = 3.1416;
+  var numero = 1.0;
+
+  print('$empleados - $pi - $numero');
+  //Strign - Carácteres
+  String nombre = 'Tony';
+  print(nombre);
+  print(nombre[0]);
+  print(nombre[nombre.length - 1]); //El arreglo empieza en 0
+}
